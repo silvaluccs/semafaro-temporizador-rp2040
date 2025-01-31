580bbae96cd9f9af944a3e8aa81e60f31f77abb8
Lucas Silva <kucasss19@gmail.com>
2025-01-31 14:17:37 -0300

    Função para inicializar os leds

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-const uint pino_led_vermelho = 11;
-const uint pino_led_azul = 12;
-const uint pino_led_verde = 13;
+const uint pino_led_vermelho = 11; // Define o pino do led vermelho
+const uint pino_led_azul = 12; // Define o pino do led azul
+const uint pino_led_verde = 13; // Define o pino do led verde
+
+void setup_led(uint pino); // Protótipo da função para inicializar um pino de led
 
 int main()
 {
-    stdio_init_all();
+    stdio_init_all(); // Inicializa a comunicação serial
+
+    setup_led(pino_led_vermelho); // Inicializa o led vermelho
+    setup_led(pino_led_azul); // Inicializa o led azul
+    setup_led(pino_led_verde); // Inicializa o led verde
 
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+/*
+* Função para inicializar um pino de led
+*/
+void setup_led(uint pino)
+{
+    gpio_init(pino); // Inicializa o pino
+    gpio_set_dir(pino, GPIO_OUT); // Define o pino como saída
+    gpio_put(pino, false);  // Desliga o led
 }
